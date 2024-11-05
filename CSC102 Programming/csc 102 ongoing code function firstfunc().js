097a6a5ad10bbacfae8cecb218fcd747b38211e8
reneop07 <reneop07@gmail.com>
2024-11-05 13:14:14 -0700

    Add files via upload

--- CSC102 Programming/csc 102 ongoing code function firstfunc().js
+++ CSC102 Programming/csc 102 ongoing code function firstfunc().js
@@ -11,14 +11,35 @@ function flipcoin()
 {
 
     var coinelement = document.getElementById ("coin");
-    var result = Math.random() <0.5 ? "Heads" : "Tails";
+    var result=Math.floor(Math.random()*2);
+    if (result == 1)
+        { document.getElementById("coin").innerHTML="Heads";
+            
+        }
+    else 
+        {
+        document.getElementById("coin").innerHTML="Tails";
+}
+   
+function validate()
+{ 
+var name = document.getElementById("initstring").value;
+var lname = document.getElementById("firstNum").value;
+var zipcod = document.getElementById("Zipcode").value;
 
-    coinelement.textContent = result;
 
+var fullname= name+lname;
+document.getElementById ("inputStatus").innerHTML =fullname;
 
-
-
+}
+if (fullname.lenght <=2)
+{
+    document.getElementById("inputStatus").innerHTML ="Name not long enough";
 
 }
 
+if (empnum <100 ) for empNum >=1000)
 
+ {
+    document.getElementById ("inputStatus") .innerHTML = "number is not 3 digits";
+ }
